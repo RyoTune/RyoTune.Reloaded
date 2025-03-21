070735ed39484f6f0389f27223b94b5e4251c98d
RyoTune <zutomayofan50@gmail.com>
2025-03-21 12:05:17 -0500

    Add docs to utils.

--- Utilities.cs
+++ Utilities.cs
@@ -2,27 +2,23 @@
 
 namespace RyoTune.Reloaded;
 
+/// <summary>
+/// Utility functionality.
+/// </summary>
 public static class Utilities
 {
+    /// <summary>
+    /// ASM for pushing caller registers.
+    /// </summary>
     public readonly static string PushCallerRegisters = "push rcx\npush rdx\npush r8\npush r9";
+
+    /// <summary>
+    /// ASM for popping caller registers.
+    /// </summary>
     public readonly static string PopCallerRegisters = "pop r9\npop r8\npop rdx\npop rcx";
+
+    /// <summary>
+    /// The current process base address.
+    /// </summary>
     public readonly static nint BaseAddress = Process.GetCurrentProcess().MainModule?.BaseAddress ?? 0;
-
-    public static ushort ToBigEndian(this ushort value)
-    {
-        var bigEndianValue = BitConverter.ToUInt16(BitConverter.GetBytes(value).Reverse().ToArray());
-        return bigEndianValue;
-    }
-
-    public static uint ToBigEndian(this uint value)
-    {
-        var bigEndianValue = BitConverter.ToUInt32(BitConverter.GetBytes(value).Reverse().ToArray());
-        return bigEndianValue;
-    }
-
-    public static short ToBigEndian(this short value)
-    {
-        var bigEndianValue = BitConverter.ToInt16(BitConverter.GetBytes(value).Reverse().ToArray());
-        return bigEndianValue;
-    }
 }
