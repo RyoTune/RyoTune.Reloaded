fbfa455a34c807296035e5a7c1097a4725301331
RyoTune <zutomayofan50@gmail.com>
2025-03-21 07:51:02 -0500

    Add doc comments and update Log.

--- Log.cs
+++ Log.cs
@@ -5,31 +5,57 @@ using System.Text;
 
 namespace RyoTune.Reloaded;
 
+/// <summary>
+/// Basic log functionality using Reloaded's logger.
+/// </summary>
 public static class Log
 {
-    private static string name = "Mod";
-    private static ILogger? log;
-    private static Color color = Color.White;
-    private static bool useAsync;
-
-    public static void Init(string name, ILogger log, bool alwaysAsync)
+    private static readonly Dictionary<LogLevel, string> _levelPrefixes = new()
     {
-        Log.name = name;
-        Log.log = log;
-        useAsync = alwaysAsync;
-        color = GetColor(name);
+        [LogLevel.Verbose] = "[VRB]",
+        [LogLevel.Debug] = "[DBG]",
+        [LogLevel.Information] = "[INF]",
+        [LogLevel.Warning] = "[WRN]",
+        [LogLevel.Error] = "[ERR]",
+    };
+
+    private static readonly Dictionary<LogLevel, Color> _levelColors = new()
+    {
+        [LogLevel.Verbose] = Color.White,
+        [LogLevel.Debug] = Color.LightGreen,
+        [LogLevel.Information] = Color.White,
+        [LogLevel.Warning] = Color.LightGoldenrodYellow,
+        [LogLevel.Error] = Color.Red,
+    };
+
+    private static string _name = "Mod";
+    private static ILogger? _log;
+    private static bool _useAsync;
+
+    internal static void Init(string name, ILogger log, bool alwaysAsync)
+    {
+        _name = name;
+        _log = log;
+        _useAsync = alwaysAsync;
+        _levelColors[LogLevel.Information] = GetColor(name);
     }
 
-    public static void Init(string name, ILogger log, Color color, bool useAsync)
+    internal static void Init(string name, ILogger log, Color color, bool useAsync)
     {
-        Log.name = name;
-        Log.log = log;
-        Log.color = color;
-        Log.useAsync = useAsync;
+        _name = name;
+        _log = log;
+        _levelColors[LogLevel.Information] = color;
+        _useAsync = useAsync;
     }
 
+    /// <summary>
+    /// Set the minimum level for logs to appear.
+    /// </summary>
     public static LogLevel LogLevel { get; set; } = LogLevel.Information;
 
+    /// <summary>
+    /// Logs that are not notable and rarely useful.
+    /// </summary>
     public static void Verbose(string message, bool useAsync = false)
     {
         if (LogLevel < LogLevel.Debug)
@@ -38,6 +64,9 @@ public static class Log
         }
     }
 
+    /// <summary>
+    /// Logs that may be useful for debugging errors.
+    /// </summary>
     public static void Debug(string message, bool useAsync = false)
     {
         if (LogLevel < LogLevel.Information)
@@ -46,6 +75,9 @@ public static class Log
         }
     }
 
+    /// <summary>
+    /// Logs that are almost always useful, such as indicating something has executed correctly.
+    /// </summary>
     public static void Information(string message, bool useAsync = false)
     {
         if (LogLevel < LogLevel.Warning)
@@ -54,6 +86,9 @@ public static class Log
         }
     }
 
+    /// <summary>
+    /// Logs indicating something incorrect or not ideal has occured, but nothing that would break mod functionality.
+    /// </summary>
     public static void Warning(string message, bool useAsync = false)
     {
         if (LogLevel < LogLevel.Error)
@@ -61,11 +96,18 @@ public static class Log
             LogMessage(LogLevel.Warning, message, useAsync);
         }
     }
+
+    /// <summary>
+    /// Logs indicating an error has occured, typically one that will break mod functionality.
+    /// </summary>
     public static void Error(Exception ex, string message, bool useAsync = false)
     {
         LogMessage(LogLevel.Error, $"{message}\n{ex.Message}\n{ex.StackTrace}", useAsync);
     }
 
+    /// <summary>
+    /// Logs indicating an error has occured, typically one that will break mod functionality.
+    /// </summary>
     public static void Error(string message, bool useAsync = false)
     {
         LogMessage(LogLevel.Error, message, useAsync);
@@ -73,34 +115,17 @@ public static class Log
 
     private static void LogMessage(LogLevel level, string message, bool useAsync = false)
     {
-        var color =
-            level == LogLevel.Debug ? Color.LightGreen :
-            level == LogLevel.Information ? Log.color :
-            level == LogLevel.Error ? Color.Red :
-            level == LogLevel.Warning ? Color.LightGoldenrodYellow :
-            Color.White;
-
-        if (useAsync || Log.useAsync)
+        if (useAsync || _useAsync)
         {
-            log?.WriteLineAsync(FormatMessage(level, message), color);
+            _log?.WriteLineAsync(FormatMessage(level, message), _levelColors[level]);
         }
         else
         {
-            log?.WriteLine(FormatMessage(level, message), color);
+            _log?.WriteLine(FormatMessage(level, message), _levelColors[level]);
         }
     }
 
-    private static string FormatMessage(LogLevel level, string message)
-    {
-        var levelStr =
-            level == LogLevel.Verbose ? "[VRB]" :
-            level == LogLevel.Debug ? "[DBG]" :
-            level == LogLevel.Information ? "[INF]" :
-            level == LogLevel.Warning ? "[WRN]" :
-            level == LogLevel.Error ? "[ERR]" : string.Empty;
-
-        return $"[{name}] {levelStr} {message}";
-    }
+    private static string FormatMessage(LogLevel level, string message) => $"[{_name}] {_levelPrefixes[level]} {message}";
 
     private static Color GetColor(string str)
     {
@@ -111,11 +136,33 @@ public static class Log
     }
 }
 
+/// <summary>
+/// Log severity level.
+/// </summary>
 public enum LogLevel
 {
+    /// <summary>
+    /// Logs that are not notable and rarely useful.
+    /// </summary>
     Verbose,
+
+    /// <summary>
+    /// Logs that may be useful for debugging errors.
+    /// </summary>
     Debug,
+
+    /// <summary>
+    /// Logs that are almost always useful, such as indicating something has executed correctly.
+    /// </summary>
     Information,
+
+    /// <summary>
+    /// Logs indicating something incorrect or not ideal has occured, but nothing that would break mod functionality.
+    /// </summary>
     Warning,
+
+    /// <summary>
+    /// Logs indicating an error has occured, typically one that will break mod functionality.
+    /// </summary>
     Error,
 }
