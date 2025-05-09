0e44b239c5a33ce075b8c0504143a3bee46d2252
RyoTune <zutomayofan50@gmail.com>
2025-05-09 16:19:14 -0500

    Set utility ASM instructions to constants.

--- Utilities.cs
+++ Utilities.cs
@@ -10,15 +10,15 @@ public static class Utilities
     /// <summary>
     /// ASM for pushing caller registers.
     /// </summary>
-    public readonly static string PushCallerRegisters = "push rcx\npush rdx\npush r8\npush r9";
+    public const string PushCallerRegisters = "push rcx\npush rdx\npush r8\npush r9";
 
     /// <summary>
     /// ASM for popping caller registers.
     /// </summary>
-    public readonly static string PopCallerRegisters = "pop r9\npop r8\npop rdx\npop rcx";
+    public const string PopCallerRegisters = "pop r9\npop r8\npop rdx\npop rcx";
 
     /// <summary>
     /// The current process base address.
     /// </summary>
-    public readonly static nint BaseAddress = Process.GetCurrentProcess().MainModule?.BaseAddress ?? 0;
+    public static readonly nint BaseAddress = Process.GetCurrentProcess().MainModule?.BaseAddress ?? 0;
 }
